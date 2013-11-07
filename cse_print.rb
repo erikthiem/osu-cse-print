765ea968a9b5c12735444e947e1ca6b4d3838ab5
Erik Thiem <erikthiem@gmail.com>
2013-11-07 11:21:45 -0500

    renamed gitignore file to correct name and queried list of printer names from text file

--- cse_print.rb
+++ cse_print.rb
@@ -1,8 +1,12 @@
 require 'net/scp'
 require 'highline/import'
 
-login = 'thiem'
-
+# This method transfers a file from the current directory to the
+# home folder of an OSU CSE student
+#
+# INPUT: login (OSU CSE login name), password (OSU CSE password), file (name of file, with extension, located in current directory)
+#
+# OUTPUT: void
 def transfer_file(login, password, file)
 	host = 'stdlinux.cse.ohio-state.edu'
 
@@ -14,6 +18,21 @@ def transfer_file(login, password, file)
 	puts 'File Transfer Ended'
 end
 
+# This method gets a list of desired printers from the
+# 'desired_printers.txt' file.
+#
+# INPUT: void
+# OUTPUT: an array of strings, each string representing an OSU CSE printer
+def get_desired_printers
+
+	printers = File.open('desired_printers.txt').read
+
+end
+
+# Get the name of the document to print
+print "Name of file to print (with extension): "
+document = gets.chomp
+
 # Get the login name
 print "Login: "
 login = gets.chomp
@@ -22,4 +41,10 @@ login = gets.chomp
 password = ask("Password: ") { |q| q.echo="*" }
 
 # Transfer the file
-transfer_file(login, password, 'test.txt')
+transfer_file(login, password, document)
+
+# Get the desired printers from the text file
+printers = get_desired_printers
+
+# Print the document
+
